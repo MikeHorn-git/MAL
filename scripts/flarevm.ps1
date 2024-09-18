6ee434b389eecc2d9a95545876ba2743204df66a
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2024-09-18 14:13:06 +0200

    Delete scripts/flarevm.ps1

--- scripts/flarevm.ps1
+++
@@ -1,8 +0,0 @@
-# Download and execute flarevm installer
-
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/mandiant/flare-vm/main/install.ps1 -OutFile install.ps1
-Unblock-File ./install.ps1
-Set-ExecutionPolicy Unrestricted -Force
-./install.ps1 -password vagrant -noWait -noGui
-
-#./install.ps1 -customConfig https://raw.githubusercontent.com/MikeHorn-git/MAL/main/config/config.xml -password vagrant -noWait -noGui
